4e6e278b501927705c221e383248f2671fdf37ef
alex_hunterz <xxalex.hunterzxx@gmail.com>
2026-05-15 22:19:01 +0530

    add guru message

--- messages.js
+++ messages.js
@@ -1,3 +1,4 @@
 const messages = [
   { name: "Manit", text: "happy birthday brother. may you get aces not only in valo but in life too. have a great one." },
+  { name: "Guru", text: "I'll molest u nosim" },
 ];
