77addedd182b337a63a372a1c78d6288a9420f6c
alex_hunterz <xxalex.hunterzxx@gmail.com>
2026-05-15 22:41:50 +0530

    add roshan message

--- messages.js
+++ messages.js
@@ -3,4 +3,5 @@ const messages = [
   { name: "Guru", text: "I'll molest u nosim" },
   { name: "Shubhang", text: "Lovely knowing you, lessen height when you talk to me 🙏🤐" },
   { name: "Sri Hari", text: "Happy Birthday, giant bro! May your K/D in life stay higher than your height." },
+  { name: "Roshan", text: "Had the most goated games with u twin! Always reliable in game and IRL. Much love 🫶 Have a great year ahead!!!" },
 ];
