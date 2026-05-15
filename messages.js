7ca74d37fac52e73491194e9af1cf7582193ccf2
alex_hunterz <xxalex.hunterzxx@gmail.com>
2026-05-15 23:38:35 +0530

    add pachisia message

--- messages.js
+++ messages.js
@@ -4,4 +4,5 @@ const messages = [
   { name: "Shubhang", text: "Lovely knowing you, lessen height when you talk to me 🙏🤐" },
   { name: "Sri Hari", text: "Happy Birthday, giant bro! May your K/D in life stay higher than your height." },
   { name: "Roshan", text: "Had the most goated games with u twin! Always reliable in game and IRL. Much love 🫶 Have a great year ahead!!!" },
+  { name: "Pachisia", text: "Happy Birthday Nosim, thanks for getting your sim finally. It's always been lovely playing all sorts of games with you my guy. Love you nigga, have a great year ahead ❤️🕕🕖" },
 ];
