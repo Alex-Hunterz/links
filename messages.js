7013eb92e145ddd17556f8b49a6e503984c5eb41
alex_hunterz <xxalex.hunterzxx@gmail.com>
2026-05-15 22:35:34 +0530

    add sri hari message

--- messages.js
+++ messages.js
@@ -2,4 +2,5 @@ const messages = [
   { name: "Manit", text: "happy birthday brother. may you get aces not only in valo but in life too. have a great one." },
   { name: "Guru", text: "I'll molest u nosim" },
   { name: "Shubhang", text: "Lovely knowing you, lessen height when you talk to me 🙏🤐" },
+  { name: "Sri Hari", text: "Happy Birthday, giant bro! May your K/D in life stay higher than your height." },
 ];
