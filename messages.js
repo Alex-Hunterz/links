fab4d594011808df105b2cbae53cdc0cd33c808d
alex_hunterz <xxalex.hunterzxx@gmail.com>
2026-05-15 22:19:19 +0530

    add shubhang message

--- messages.js
+++ messages.js
@@ -1,4 +1,5 @@
 const messages = [
   { name: "Manit", text: "happy birthday brother. may you get aces not only in valo but in life too. have a great one." },
   { name: "Guru", text: "I'll molest u nosim" },
+  { name: "Shubhang", text: "Lovely knowing you, lessen height when you talk to me 🙏🤐" },
 ];
